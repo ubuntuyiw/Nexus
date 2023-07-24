a1b4466b87cc565a1a34fa8f8f927e323ae504da
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2023-07-24 15:56:28 +0300

    Navigation Compose dependencies added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,4 +81,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.6.0")
 }
