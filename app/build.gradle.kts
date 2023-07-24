622bd726a93011dfea5b84c81a1704bc7cfd386c
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2023-07-24 16:04:06 +0300

    Retrofit dependencies added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,4 +101,8 @@ dependencies {
     //Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+
 }
