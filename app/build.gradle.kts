5a5ffb34a2ae905260847c8aafc98853576c36dd
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2023-07-24 16:04:25 +0300

    OkHttp3 dependencies added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,4 +105,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 
+    //OkHttp3
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
 }
