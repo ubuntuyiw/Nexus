a17c15fe105e0e031794b231c137828aca4d91c9
İBRAHİM KURT <ibrahim.kurt@ubuntuyouiwe.com>
2023-08-06 02:32:46 +0300

    Added icons, fonts and updated theme & string.xml

--- app/src/main/java/com/ubuntuyouiwe/nexus/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/ubuntuyouiwe/nexus/presentation/ui/theme/Color.kt
@@ -4,8 +4,11 @@ import androidx.compose.ui.graphics.Color
 
 val DeepBlueSea = Color(0xFF032541)
 val BrightCerulean = Color(0xFF2181CF)
+val Black = Color(0xFF000000)
+val DarkGray = Color(0xFF444444)
+val Gray = Color(0xFF888888)
 val Whisper = Color(0xFFF6F6F6)
-val White = Color.White
-val Black = Color.Black
-val Gray = Color.Gray
 val SoftGray = Color(0xFFEAEAEA)
+val VeryLightGray = Color(0xFFEEEEEE)
+val White = Color(0xFFFFFFFF)
+val gradientColors = listOf(DeepBlueSea, BrightCerulean)
--- app/src/main/java/com/ubuntuyouiwe/nexus/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/ubuntuyouiwe/nexus/presentation/ui/theme/Type.kt
@@ -1,6 +1,10 @@
 package com.ubuntuyouiwe.nexus.presentation.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -9,13 +13,26 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.ubuntuyouiwe.nexus.R
 
-// Set of Material typography styles to start with
+
+@OptIn(ExperimentalTextApi::class)
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily(Font(R.font.roboto_medium)),
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.5.sp,
+
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp,
+
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
+        fontSize = 13.sp,
+        letterSpacing = 0.sp,
+
     ),
     displayLarge = TextStyle(
         fontFamily = FontFamily(Font(R.font.rosmatika_regular)),
@@ -23,10 +40,13 @@ val Typography = Typography(
         letterSpacing = 16.sp
 
     ),
+
     displayMedium = TextStyle(
         fontFamily = FontFamily(Font(R.font.rosmatika_regular)),
         fontSize = 28.sp,
-        letterSpacing = 8.sp
+        letterSpacing = 8.sp,
+        brush = Brush.linearGradient(
+            colors = gradientColors)
 
     ),
     headlineSmall = TextStyle(
@@ -35,28 +55,20 @@ val Typography = Typography(
         letterSpacing = 2.sp,
 
     ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
+        fontSize = 12.sp,
+        textAlign = TextAlign.Center,
+        color = Gray,
+        lineHeight = 18.sp
+
+    ),
     labelSmall = TextStyle(
         fontFamily = FontFamily(Font(R.font.rosmatika_regular)),
         fontSize = 11.sp,
         textAlign = TextAlign.Center,
         letterSpacing = 2.sp,
-        lineHeight = 24.sp,
+        lineHeight = 24.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 
 )
